e45b414c9a1606af421d9312a3844206861450e9
homebeaver <eugen.hanussek@klst.com>
2017-02-08 18:57:06 +0100

    TODO implemet DispatchNotificationProcess

--- de.metas.endcustomer.mf15/de.metas.endcustomer.mf15.swingui/src/main/java/com/klst/opentrans/XmlReader.java
+++ de.metas.endcustomer.mf15/de.metas.endcustomer.mf15.swingui/src/main/java/com/klst/opentrans/XmlReader.java
@@ -59,7 +59,7 @@ import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-import com.klst.minhoff.opentrans.process.AvisPipedInputStream;
+import com.klst.mf.opentrans.process.AvisPipedInputStream;
 
 import de.metas.logging.LogManager;
 
